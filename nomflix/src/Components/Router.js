e132844cb4fd9c08b70f6c4002379124c430c6d1
김민수 <alstn2468_@naver.com>
2019-08-13 01:23:43 +0900

    Complete using styled-component

--- nomflix/src/Components/Router.js
+++ nomflix/src/Components/Router.js
@@ -8,9 +8,11 @@ import {
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
+import Header from "Components/Header";
 
 export default () => (
     <Router>
+        <Header />
         <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/tv" component={TV} />
