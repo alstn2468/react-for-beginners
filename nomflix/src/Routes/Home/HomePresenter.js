3f1bf989bef635677a307bafb95cc0e4c258c63d
김민수 <alstn2468_@naver.com>
2019-08-14 00:20:28 +0900

    Create Section.js && Create Home component using Section

--- nomflix/src/Routes/Home/HomePresenter.js
+++ nomflix/src/Routes/Home/HomePresenter.js
@@ -1,9 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Section from "Components/Section";
+
+const Container = styled.div`
+  padding: 0px 10px;
+`;
 
 const HomePresenter = ({ nowPlaying, upcoming, popular, loading, error }) =>
-  null;
+  loading ? null : (
+    <Container>
+      {nowPlaying && nowPlaying.length > 0 && (
+        <Section title="Now Playing">
+          {nowPlaying.map(movie => movie.title)}
+        </Section>
+      )}
+
+      {upcoming && upcoming.length > 0 && (
+        <Section title="Upcoming Movie">
+          {upcoming.map(movie => movie.title)}
+        </Section>
+      )}
+
+      {popular && popular.length > 0 && (
+        <Section title="Popular Movie">
+          {popular.map(movie => movie.title)}
+        </Section>
+      )}
+    </Container>
+  );
 
 HomePresenter.propTypes = {
   nowPlaying: PropTypes.array,
