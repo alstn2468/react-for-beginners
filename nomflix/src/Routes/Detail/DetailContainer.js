eda25c86073a68717fb3f035216b62866fd1a851
김민수 <alstn2468_@naver.com>
2019-08-13 23:05:06 +0900

    Finish all routes Container.js

--- nomflix/src/Routes/Detail/DetailContainer.js
+++ nomflix/src/Routes/Detail/DetailContainer.js
@@ -1,12 +1,20 @@
 import React from "react";
 import DetailPresenter from "./DetailPresenter";
+import { moviesApi, tvApi } from "api";
 
 export default class extends React.Component {
-  state = {
-    result: null,
-    error: null,
-    loading: true
-  };
+  constructor(props) {
+    super(props);
+    const {
+      location: { pathname }
+    } = props;
+    this.state = {
+      result: null,
+      error: null,
+      loading: true,
+      isMovie: pathname.includes("/movie/")
+    };
+  }
 
   async componentDidMount() {
     const {
@@ -16,17 +24,32 @@ export default class extends React.Component {
       history: { push }
     } = this.props;
 
+    const { isMovie } = this.state;
     const parsedId = parseInt(id);
 
     if (isNaN(parsedId)) {
       return push("/");
     }
 
-    console.log();
+    let result = null;
+
+    try {
+      if (isMovie) {
+        ({ data: result } = await moviesApi.movieDetail(parsedId));
+      } else {
+        ({ data: result } = await tvApi.showDetail(parsedId));
+      }
+    } catch {
+      this.setState({ error: "Can't find anything." });
+    } finally {
+      this.setState({ loading: false, result });
+    }
   }
 
   render() {
     const { result, error, loading } = this.state;
+
+    console.log(result);
 
     return <DetailPresenter result={result} error={error} loading={loading} />;
   }
