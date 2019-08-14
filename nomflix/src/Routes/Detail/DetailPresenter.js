6642b4de9a8e5697778e3996184b76fa26d20cb1
김민수 <alstn2468_@naver.com>
2019-08-15 01:04:02 +0900

    Write DetailPresenter without watching videos

--- nomflix/src/Routes/Detail/DetailPresenter.js
+++ nomflix/src/Routes/Detail/DetailPresenter.js
@@ -1,8 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Loader from "Components/Loader";
 
-const DetailPresenter = ({ result, error, loading }) => null;
+const DetailPresenter = ({ result, error, loading }) =>
+  loading ? <Loader /> : <p>{result.original_title}</p>;
 
 DetailPresenter.propTypes = {
   result: PropTypes.object,
