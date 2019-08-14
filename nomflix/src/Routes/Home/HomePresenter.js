ec04398750fd1eb384590f83d5f2d64618e2ac00
김민수 <alstn2468_@naver.com>
2019-08-14 17:28:27 +0900

    Add Poster component & get all props for poseter & add style sheet

--- nomflix/src/Routes/Home/HomePresenter.js
+++ nomflix/src/Routes/Home/HomePresenter.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import Section from "Components/Section";
 import Loader from "Components/Loader";
 import Message from "Components/Message";
+import Poster from "Components/Poster";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -17,7 +18,15 @@ const HomePresenter = ({ nowPlaying, upcoming, popular, loading, error }) =>
       {nowPlaying && nowPlaying.length > 0 && (
         <Section title="Now Playing">
           {nowPlaying.map(movie => (
-            <span key={movie.id}>{movie.title}</span>
+            <Poster
+              key={movie.id}
+              id={movie.id}
+              imageUrl={movie.poster_path}
+              title={movie.original_title}
+              year={movie.release_date.substring(0, 4)}
+              rating={movie.vote_average}
+              isMovie={true}
+            />
           ))}
         </Section>
       )}
@@ -25,7 +34,15 @@ const HomePresenter = ({ nowPlaying, upcoming, popular, loading, error }) =>
       {upcoming && upcoming.length > 0 && (
         <Section title="Upcoming Movie">
           {upcoming.map(movie => (
-            <span key={movie.id}>{movie.title}</span>
+            <Poster
+              key={movie.id}
+              id={movie.id}
+              imageUrl={movie.poster_path}
+              title={movie.original_title}
+              year={movie.release_date.substring(0, 4)}
+              rating={movie.vote_average}
+              isMovie={true}
+            />
           ))}
         </Section>
       )}
@@ -33,7 +50,15 @@ const HomePresenter = ({ nowPlaying, upcoming, popular, loading, error }) =>
       {popular && popular.length > 0 && (
         <Section title="Popular Movie">
           {popular.map(movie => (
-            <span key={movie.id}>{movie.title}</span>
+            <Poster
+              key={movie.id}
+              id={movie.id}
+              imageUrl={movie.poster_path}
+              title={movie.original_title}
+              year={movie.release_date.substring(0, 4)}
+              rating={movie.vote_average}
+              isMovie={true}
+            />
           ))}
         </Section>
       )}
