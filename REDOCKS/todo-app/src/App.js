fbaab3f4142bbba43130766677c114819abc12d7
김민수 <alstn2468_@naver.com>
2019-08-18 18:01:59 +0900

    Complete useReducer example in React.js

--- REDOCKS/todo-app/src/App.js
+++ REDOCKS/todo-app/src/App.js
@@ -1,22 +1,28 @@
-import React from "react";
+import React, { useReducer } from "react";
+
+const INCREMENT = "increment";
+const DECREMENT = "decrement";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return { count: state.count + 1 };
+    case DECREMENT:
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  console.log(dispatch);
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>{state.count}</h1>
+      <button onClick={() => dispatch({ type: INCREMENT })}>Add</button>
+      <button onClick={() => dispatch({ type: DECREMENT })}>Remove</button>
+    </>
   );
 }
 
