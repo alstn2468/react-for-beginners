dbbd0f64699b809cd8bc6c1f8609983915256a6b
김민수 <alstn2468_@naver.com>
2019-08-18 13:38:25 +0900

    Add Input.tsx using event in React with TypeScript

--- TypeScript/typescript-react/src/App.tsx
+++ TypeScript/typescript-react/src/App.tsx
@@ -1,25 +1,39 @@
 import React, { Component } from "react";
 import Number from "./Number";
+import { Input, Form } from "./Input";
 
 interface IState {
   counter: number;
+  name: string;
 }
 
 class App extends Component<{}, IState> {
   state = {
-    counter: 0
+    counter: 0,
+    name: ""
   };
 
   render() {
-    const { counter } = this.state;
+    const { counter, name } = this.state;
 
     return (
       <div>
+        <Form onFormSubmit={this.onFormSubmit}>
+          <Input value={name} onChange={this.onChange} />
+        </Form>
         <Number count={counter} />
         <button onClick={this.add}>Add</button>
       </div>
     );
   }
+
+  onChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    console.log(event.target);
+  };
+
+  onFormSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+  };
 
   add = () => {
     // this.setState({ counter: "hello" });
