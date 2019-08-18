f3519229964ec344c81f30ddc870fcb23edcd508
김민수 <alstn2468_@naver.com>
2019-08-18 16:31:39 +0900

    Complete using useContext function in react:

--- REDOCKS/hypertranslate-app/src/App.js
+++ REDOCKS/hypertranslate-app/src/App.js
@@ -1,23 +1,13 @@
 import React from "react";
-import "./App.css";
+import Screen from "./Screen";
+import Lang from "./context";
+import translations from "./translations";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Lang defaultLang="en" translations={translations}>
+      <Screen />
+    </Lang>
   );
 }
 
